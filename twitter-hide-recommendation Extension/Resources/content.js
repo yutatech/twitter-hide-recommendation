7e4c14999d8491784b449b9d4da75fceb1cc1592
Yuta Fujiyama <yuta.technology@gmail.com>
2025-04-10 02:15:52 +0900

    hide recomendation

--- twitter-hide-recommendation Extension/Resources/content.js
+++ twitter-hide-recommendation Extension/Resources/content.js
@@ -1,7 +1,27 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+// browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+//     console.log("Received response: ", response);
+// });
+
+// browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     console.log("Received request: ", request);
+// });
+
+const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+        // DOMツリーが更新されたときに実行
+        const divsWithRole = document.querySelectorAll('div[role="presentation"]');
+        divsWithRole.forEach(el => {
+            // 「おすすめ」のみj列が含まれるelを非表示にする
+            if (el.textContent.includes('おすすめ')) {
+                el.style.display = 'none';
+//                observer.disconnect();
+            }
+        });
+    }
 });
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
 });
