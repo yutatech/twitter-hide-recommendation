c385d2d3c68619f7588a27b255492bda35dafcb3
Yuta Fujiyama <yuta.technology@gmail.com>
2025-04-09 10:56:03 +0900

    Initial Commit

---
+++ twitter-hide-recommendation/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  twitter-hide-recommendation
+//
+//  Created by 藤山優太 on 2025/04/09.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
